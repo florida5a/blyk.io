98138e4c9574ca1632efb41ddd8787d0a22206e7
florida5a <fr.kbrf@gmail.com>
2026-07-10 15:39:43 +0300

    Update app.py

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ if uploaded_file is not None:
                     """
                     
                     # Вызываем актуальную модель напрямую через библиотеку Google
-                    model = genai.GenerativeModel('gemini-1.5-flash')
+                    model = genai.GenerativeModel('gemini-2.0-flash')
                     response = model.generate_content([prompt, image])
                     
                     st.success("Анализ Blyk.io завершен!")
